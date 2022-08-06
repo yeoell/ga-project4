82996fac60a9ebc26861b68c03ab61ce9327772f
Eleanor Yeoell <eleanoryeoell@icloud.com>
2022-08-06 12:00:45 +1000

    process refinement

--- client/src/Edit.js
+++ client/src/Edit.js
@@ -1,17 +1,68 @@
 import { useState, useEffect } from "react";
-import { SourcingTasks } from "./TaskDescriptions";
+import {
+  PlanTasks,
+  SourcingTasks,
+  BuildTasks,
+  ReleaseTasks,
+  SupportTasks,
+} from "./TaskDescriptions";
 import "./Tasks.css";
 import "./Workflow.css";
 
 function Edit() {
-  //   const [editedSourcingTasks, setSourcingTasks] = useState([]);
-  const [isDeleted, setDeleted] = useState(false);
-  const [updatedSourcing, setUpdates] = useState([]);
+  //plan
+  const [isUpdatedPlan, setPlanDeleted] = useState(false);
+  const [updatedPlan, setPlanUpdates] = useState([]);
+  const removePlanTask = (id) => {
+    const newTasks = PlanTasks.filter((job) => job.id !== id);
+    setPlanUpdates(newTasks);
+  };
+  const handlePlanClick = () => {
+    setPlanDeleted((current) => !current);
+  };
 
-  const removeTask = (id) => {
+  //create
+  const [isUpdatedSource, setSourceDeleted] = useState(false);
+  const [updatedSourcing, setUpdates] = useState([]);
+  const removeSourceTask = (id) => {
     const newTasks = SourcingTasks.filter((job) => job.id !== id);
     setUpdates(newTasks);
-    console.log(updatedSourcing);
+  };
+  const handleSourceClick = () => {
+    setSourceDeleted((current) => !current);
+  };
+
+  //build
+  const [isUpdatedBuild, setBuildDeleted] = useState(false);
+  const [updatedBuild, setBuildUpdates] = useState([]);
+  const removeBuildTask = (id) => {
+    const newTasks = BuildTasks.filter((job) => job.id !== id);
+    setBuildUpdates(newTasks);
+  };
+  const handleBuildClick = () => {
+    setBuildDeleted((current) => !current);
+  };
+
+  //release
+  const [isUpdatedRelease, setReleaseDeleted] = useState(false);
+  const [updatedRelease, setReleaseUpdates] = useState([]);
+  const removeReleaseTask = (id) => {
+    const newTasks = ReleaseTasks.filter((job) => job.id !== id);
+    setReleaseUpdates(newTasks);
+  };
+  const handleReleaseClick = () => {
+    setReleaseDeleted((current) => !current);
+  };
+
+  //support
+  const [isUpdatedSupport, setSupportDeleted] = useState(false);
+  const [updatedSupport, setSupportUpdates] = useState([]);
+  const removeSupportTask = (id) => {
+    const newTasks = SupportTasks.filter((job) => job.id !== id);
+    setSupportUpdates(newTasks);
+  };
+  const handleSupportClick = () => {
+    setSupportDeleted((current) => !current);
   };
 
   return (
@@ -38,7 +89,29 @@ function Edit() {
             <em>support</em>
           </div>
           <div></div>
-          <div className="stage process-box"></div>
+          <div className="stage process-box">
+            <div>
+              <h4>Product planning</h4>
+              <ul>
+                {PlanTasks.map((task) => {
+                  return (
+                    <div key={task.id} className="task-box">
+                      <li
+                        className="remove"
+                        onClick={() => {
+                          removePlanTask(task.id);
+                          handlePlanClick();
+                        }}
+                        style={{ textDecoration: isUpdatedPlan ? "line-through" : "" }}
+                      >
+                        {task.task}
+                      </li>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
           <div className="stage process-box">
             <div>
               <h4>Sourcing</h4>
@@ -49,9 +122,10 @@ function Edit() {
                       <li
                         className="remove"
                         onClick={() => {
-                          removeTask(task.id);
+                          removeSourceTask(task.id);
+                          handleSourceClick();
                         }}
-                        style={{ textDecoration: isDeleted ? "line-through" : "" }}
+                        style={{ textDecoration: isUpdatedSource ? "line-through" : "" }}
                       >
                         {task.task}
                       </li>
@@ -61,16 +135,95 @@ function Edit() {
               </ul>
             </div>
           </div>
-          <div className="stage"></div>
-          <div className="stage"></div>
-          <div className="stage"></div>
+          <div className="stage process-box">
+            <div>
+              <h4>Content composition</h4>
+              <ul>
+                {BuildTasks.map((task) => {
+                  return (
+                    <div key={task.id} className="task-box">
+                      <li
+                        className="remove"
+                        onClick={() => {
+                          removeBuildTask(task.id);
+                          handleBuildClick();
+                        }}
+                        style={{ textDecoration: isUpdatedBuild ? "line-through" : "" }}
+                      >
+                        {task.task}
+                      </li>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="stage process-box">
+            <div>
+              <h4>Release</h4>
+              <ul>
+                {ReleaseTasks.map((task) => {
+                  return (
+                    <div key={task.id} className="task-box">
+                      <li
+                        className="remove"
+                        onClick={() => {
+                          removeReleaseTask(task.id);
+                          handleReleaseClick();
+                        }}
+                        style={{ textDecoration: isUpdatedRelease ? "line-through" : "" }}
+                      >
+                        {task.task}
+                      </li>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="stage process-box">
+            <div>
+              <h4>Continuous improvement</h4>
+              <ul>
+                {SupportTasks.map((task) => {
+                  return (
+                    <div key={task.id} className="task-box">
+                      <li
+                        className="remove"
+                        onClick={() => {
+                          removeSupportTask(task.id);
+                          handleSupportClick();
+                        }}
+                        style={{ textDecoration: isUpdatedSupport ? "line-through" : "" }}
+                      >
+                        {task.task}
+                      </li>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
           <div></div>
         </div>
         <div>
           <h3>suggested refinement</h3>
         </div>
         <div className="container">
-          <div className="stage"></div>
+          <div className="stage process-box">
+            <div>
+              <h4>Sourcing</h4>
+              <ul>
+                {updatedPlan.map((task) => {
+                  return (
+                    <div key={task.id} className="task-box">
+                      <li>{task.task}</li>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
           <div className="stage process-box">
             <div>
               <h4>Sourcing</h4>
@@ -85,9 +238,48 @@ function Edit() {
               </ul>
             </div>
           </div>
-          <div className="stage"></div>
-          <div className="stage"></div>
-          <div className="stage"></div>
+          <div className="stage process-box">
+            <div>
+              <h4>Sourcing</h4>
+              <ul>
+                {updatedBuild.map((task) => {
+                  return (
+                    <div key={task.id} className="task-box">
+                      <li>{task.task}</li>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="stage">
+            <div>
+              <h4>Release</h4>
+              <ul>
+                {updatedRelease.map((task) => {
+                  return (
+                    <div key={task.id} className="task-box">
+                      <li>{task.task}</li>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="stage">
+            <div>
+              <h4>Continuous improvement</h4>
+              <ul>
+                {updatedSupport.map((task) => {
+                  return (
+                    <div key={task.id} className="task-box">
+                      <li>{task.task}</li>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
           <div></div>
         </div>
       </div>
